194473ff35f7f627f1a47ac5ec9e5cf4ccd63864
Adnan Ahmed <adnan.aant@gmail.com>
2022-06-29 21:38:56 +0100

    feat: Added util folders for later use and removed css file

---
+++ utils/common.ts
@@ -0,0 +1,13 @@
+export const regions = {
+  euw: "euw1",
+  eune: "eun1",
+  na: "na1",
+  jp: "jp1",
+  lan: "la1",
+  las: "la2",
+  oce: "oc1",
+  kr: "kr",
+  ru: "ru1",
+  tr: "tr1",
+  br: "br1",
+}
