cef8e06f02296d0275353bdb21e0bdb2835e784d
Adnan Ahmed <adnan.aant@gmail.com>
2022-06-29 21:31:38 +0100

    feat: Removed logic on _app.tsx component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,9 @@ import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Navbar from './components/Navbar'
 
-const darkMode = true
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [minimiseNav, setMinimiseNav] = useState(false)
-  const [isDarkMode, setisDarkMode] = useState(true)
-  useEffect(() => {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) || darkMode) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  }, [])
   return (
 
     <Component {...pageProps} />
