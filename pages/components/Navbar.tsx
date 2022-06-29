3fb185c6feac781607e6dd0ec80e3ef10789508d
fzippy <hk2011khan@gmail.com>
2022-06-29 21:55:01 +0100

    feat: merged nav

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { GiBarbute } from "react-icons/gi";
+import { AiTwotoneHome } from "react-icons/ai";
 
 interface NavbarProps {
   isMinimized: boolean
@@ -8,7 +10,20 @@ const Navbar = ({ isMinimized }: NavbarProps) => {
 
   return (
     <div className={`${!isMinimized ? "w-64" : "w-16"} min-h-screen bg-primary`}>
-    </div >
+      <div className='text-4xl text-slate-400'>viewLeague</div>
+      <div className='flex p-3  items-center my-8'>
+        <div className='text-slate-200 '>
+          <AiTwotoneHome size={32}/>
+        </div>
+        <div className='text-xl text-slate-400 pl-3'>Home</div>
+      </div>
+      <div className='flex p-3  items-center my-8'>
+        <div className='text-slate-200 '>
+          <GiBarbute size={32}/>
+        </div>
+        <div className='text-xl text-slate-400 pl-3'>Champions</div>
+      </div>
+    </div>
   )
 }
 
