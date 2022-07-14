0f5605f1d7f6c856df867e6498116b03f7352378
Adnan Ahmed <adnan.aant@gmail.com>
2022-07-15 00:09:15 +0100

    Added continent value to the region objects

--- src/utils/commonData.ts
+++ src/utils/commonData.ts
@@ -1,46 +1,59 @@
-export const regions = {
+import { IRegions } from "../types/types";
+
+export const regions: IRegions = {
   euw: {
     value: "euw1",
-    regionName: "Europe"
+    regionName: "Europe",
+    continent: "EUROPE"
   },
   eune: {
     value: "eun1",
-    regionName: "Europe Nordic"
+    regionName: "Europe Nordic",
+    continent: "EUROPE"
   },
   na: {
     value: "na1",
-    regionName: "North America"
+    regionName: "North America",
+    continent: "AMERICAS"
   },
   jp: {
     value: "jp1",
-    regionName: "Japan"
+    regionName: "Japan",
+    continent: "ASIA"
   },
   lan: {
     value: "la1",
-    regionName: "LAN"
+    regionName: "LAN",
+    continent: "AMERICAS"
   },
   las: {
     value: "la2",
-    regionName: "LAS"
+    regionName: "LAS",
+    continent: "AMERICAS"
   },
   oce: {
     value: "oc1",
-    regionName: "Oceania"
+    regionName: "Oceania",
+    continent: "SEA"
   },
   kr: {
     value: "kr",
-    regionName: "Korea"
+    regionName: "Korea",
+    continent: "ASIA"
   },
   ru: {
     value: "ru1",
-    regionName: "Russia"
+    regionName: "Russia",
+    continent: "EUROPE"
   },
   tr: {
     value: "tr1",
-    regionName: "Turkey"
+    regionName: "Turkey",
+    continent: "EUROPE"
   },
   br: {
     value: "br1",
-    regionName: "Brazil"
+    regionName: "Brazil",
+    continent: "AMERICAS"
   },
 }
