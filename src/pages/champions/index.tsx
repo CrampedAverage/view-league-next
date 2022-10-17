262817c3665f0ff921edac26c7fc7d2fc38f33e1
CrampedAverage <adnan.aant@gmail.com>
2022-10-18 00:34:16 +0100

    Completed implementation of champion page

--- src/pages/champions/index.tsx
+++ src/pages/champions/index.tsx
@@ -1,24 +1,32 @@
-import axios from 'axios'
-import { NextPage } from 'next'
-import Head from 'next/head'
-import { type } from 'os'
-import React, { useEffect, useState } from 'react'
-import Home from '../../components/Home/Home'
-import Layout from '../../components/shared/Layout'
+import axios from "axios";
+import { NextPage } from "next";
+import Head from "next/head";
+import { type } from "os";
+import React, { useEffect, useState } from "react";
+import Home from "../../components/Home/Home";
+import Layout from "../../components/shared/Layout";
 
-interface ChampionType {
-  name: string
+interface TChampion {
+  name: string;
   image: {
-    full: string
-  }
+    full: string;
+  };
 }
 
-interface ChampionListType {
-  [key: string]: ChampionType
+interface TChampionList {
+  data: {
+    [key: string]: TChampion;
+  };
 }
 
-const Champions: NextPage = ({ championsList }: ChampionListType) => {
+interface ChampionsProp {
+  championsList: TChampionList;
+}
 
+const Champions = ({ championsList }: ChampionsProp) => {
+  useEffect(() => {
+    console.log(championsList);
+  });
   return (
     <Layout>
       <Head>
@@ -26,34 +34,35 @@ const Champions: NextPage = ({ championsList }: ChampionListType) => {
         <meta name="description" content="Search for any league Player" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='flex-col w-3/4 mx-auto max-h-screen overflow-y-scroll '>
-        <p className='text-center text-5xl  '>Champions</p>
-        <p className='text-center text-2xl  '>click for champion info</p>
-        <ul className='flex flex-wrap p-6 justify-center '>
-
-          {Object.values(championsList).map((champs) => {
+      <div className="flex-col w-3/4 mx-auto max-h-screen overflow-y-scroll ">
+        <p className="text-center text-5xl">Champions</p>
+        <p className="text-center text-2xl">click for champion info</p>
+        <ul className="flex flex-wrap p-6 justify-center ">
+          {Object.values(championsList.data).map((champion: TChampion) => {
             return (
-              <li className='flex-col w-28 h-28 border-2 border-solid dark:border-primary m-5'>
-                <img className='w-full h-4/5' src={`http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/${champs.image.full}`}></img>
-                <p className='text-center  '>{champs.name}</p>
+              <li className="flex-col w-28 h-28 border-2 border-solid dark:border-primary m-5">
+                <img
+                  className="w-full h-4/5"
+                  src={`http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/${champion.image.full}`}
+                ></img>
+                <p className="text-center overflow-ellipsis">{champion.name}</p>
               </li>
-            )
+            );
           })}
         </ul>
       </div>
     </Layout>
-  )
-}
+  );
+};
 export async function getStaticProps() {
-  const champions = await axios.get("http://ddragon.leagueoflegends.com/cdn/11.8.1/data/en_US/champion.json")
-  const championList = champions.data
+  const champions = await axios.get("http://127.0.0.1:4040/champions");
+  const championList: TChampionList = champions.data;
+
   return {
     props: {
       championsList: championList,
-    }
-  }
-
+    },
+  };
 }
 
-
-export default Champions
+export default Champions;
