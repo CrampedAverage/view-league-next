24e3235bc833ac6586cd4621a92118a4ccb9bf78
CrampedAverage <adnan.aant@gmail.com>
2022-10-29 16:30:10 +0100

    Refactored Layout file

--- src/components/Summoner/404.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../../components/shared/Layout";
-
-const PlayerNotFound = () => {
-  return <Layout>PlayerNotFound</Layout>;
-};
-
-export default PlayerNotFound;
