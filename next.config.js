e797957eac9b9a77924bbedb56592aae5d2c1853
CrampedAverage <adnan.aant@gmail.com>
2022-10-30 21:46:12 +0000

    Added base path to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["ddragon.leagueoflegends.com"],
   },
   experimental: { appDir: true },
+  basePath: "/src",
 };
 
 module.exports = nextConfig;
