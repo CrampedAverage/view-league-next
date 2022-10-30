6e3c5680a013ff7836ce386bc44e8a8c2329f84b
CrampedAverage <adnan.aant@gmail.com>
2022-10-30 22:13:57 +0000

    Changing variable naming

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,12 +1,5 @@
 import axios from "axios";
-import { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { type } from "os";
-import React, { use, useEffect, useState } from "react";
 import Champions from "../../components/Pages/Champions/Champions";
-import Home from "../../components/Pages/Home/Home";
-import Layout from "../../components/shared/Layout";
 
 export interface TChampion {
   name: string;
@@ -22,13 +15,13 @@ export interface TChampionList {
 }
 
 const getChampions = async () => {
-  const champions = await axios.get("http://127.0.0.1:4040/api/champions");
-  const championList: TChampionList = champions.data;
+  const response = await axios.get("http://127.0.0.1:4040/api/champions");
+  const champions: TChampionList = response.data;
 
-  return championList;
+  return champions;
 };
 
 export default async function ChampionsPage() {
-  const championList = await getChampions();
-  return <Champions championList={championList} />;
+  const champions = await getChampions();
+  return <Champions champions={champions} />;
 }
